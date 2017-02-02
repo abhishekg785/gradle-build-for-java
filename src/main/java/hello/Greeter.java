c513467340c3ea8d233236a2d3bbd6014f372838
abhishekg785 <abhishekg785@gmail.com>
2017-02-02 21:50:12 +0530

    gradle added to the simple project

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,12 @@
+package main.java.hello;
+
+/**
+ * Created by hiro on 2/2/17.
+ */
+public class Greeter {
+
+    public String sayHello() {
+        return "Hello World !";
+    }
+
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,19 @@
+package main.java.hello;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Created by hiro on 2/2/17.
+ *
+ * getting current time using joda lib which dependency has been defined in
+ * the build.gradle file and will be handled from there making our life easy :)
+ */
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current time is :" + currentTime);
+        Greeter greeter = new Greeter();
+        greeter.sayHello();
+    }
+}
